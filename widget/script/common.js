26ccaf394ff5daa40b9116adc6457b9d4be17789
j0o0oker <1716081017@qq.com>
2020-01-02 10:42:54 +0800

    articles, rich text

--- widget/script/common.js
+++ widget/script/common.js
@@ -2,6 +2,7 @@ var j = {};
 
 
 // 打开窗口
+
 j.openWin = function (url, param) {
     api.openWin({
         name: 'page1',
@@ -9,9 +10,75 @@ j.openWin = function (url, param) {
         pageParam: param
     });
 }
+j.openWinNav = function (url, param, title) {
+    var name = url.slice(url.lastIndexOf('/') + 1, url.lastIndexOf('.'));
+    api.openTabLayout({
+        name: name,
+        url: url,
+        title: title,
+        pageParam: param,
+        hideNavigationBar: false,
+        navigationBar: {
+            height: 45,                  //（可选项）导航栏高度。默认值45。数字类型
+            background: '#fff',             //（可选项）导航栏背景。支持颜色值和图片，默认值#fff，字符串类型
+            shadow: '#fff',                  //（可选项）导航栏底部阴影线颜色。默认值#ddd，字符串类型
+            color: '#000',
+            leftButtons: [{
+                iconPath: 'widget://image/post/black_back_arrow.png'
+            }],
+            rightButtons: [{
+                iconPath: 'widget://image/post/dh.png'
+            }]
+
+        }
+    });
+}
 
 
 
+
+//公共功能
+/*
+*   获取当前经纬度
+*   param: {
+        callback: function (lat, lon) {
+                    lat: '维度'， lon：'经度'
+                  }
+*   }
+*/
+j.getCurLocation = function (callback) {
+    var resultList = api.hasPermission({
+        list:['location']
+    });
+    if (resultList[0].granted === false) {
+        j.log('没有定位权限');
+    }
+    api.requestPermission({
+        list:['location'],
+        code:1
+    }, function(ret, err) {
+        if (err) {
+            j.toast('获取定位权限失败');
+            return;
+        }
+        var resultList = api.hasPermission({
+            list:['location']
+        });
+        if (resultList[0].granted === false) {
+            j.log('没有定位权限');
+        }
+        api.getLocation(function(ret, err) {
+            if (err) {
+                j.toast('获取当前地理位置信息失败');
+                return;
+            }
+            if (ret && ret.status) {
+                //ret.timestamp
+                callback(ret.latitude, ret.longitude);
+            }
+        });
+    });
+}
 
 
 
@@ -20,8 +87,11 @@ j.openWin = function (url, param) {
 *打印
 */
 j.log = function (content) {
-    var result;
-    if (typeof content === Object) {
+    if (typeof content === 'object' || typeof content === Object) {
+        console.log(JSON.stringify(content));
+        return;
+    }
+    if (typeof content === Array || typeof content === 'array') {
         console.log(JSON.stringify(content));
         return;
     }
